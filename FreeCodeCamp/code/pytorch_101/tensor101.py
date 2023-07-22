92f3bf12c9cc1f794dbb9f31ad60a10e061e895d
aditya-github-as88 <139616413+aditya-github-as88@users.noreply.github.com>
2023-07-23 03:01:24 +0530

    code commit to update Tensor documentation
    Till basic operation sin tensors

---
+++ FreeCodeCamp/code/pytorch_101/tensor101.py
@@ -0,0 +1,15 @@
+import torch
+class Tensor101(object):
+    def __init__(self):
+        pass
+    
+    def test(self):
+        print('Aditya')
+        
+
+def main():
+    obj = Tensor101()
+    obj.test()
+        
+if __name__ == '__main__':
+    main()
