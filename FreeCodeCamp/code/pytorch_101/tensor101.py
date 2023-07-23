23569d2f4aac2067ae2882ffe845290a66fce8cf
aditya-github-as88 <139616413+aditya-github-as88@users.noreply.github.com>
2023-07-23 22:04:49 +0530

    checking in word doc
    Added tensor types , zeros and ones and few more function

--- FreeCodeCamp/code/pytorch_101/tensor101.py
+++ FreeCodeCamp/code/pytorch_101/tensor101.py
@@ -6,6 +6,28 @@ class Tensor101(object):
     def test(self):
         print('Aditya')
         
+    def getTensorTypes():
+        # function to get all types of Tensors ( scaler , vector , Matrix , Tensor(Multi-dim))
+        # random Tensors
+        pass
+    
+    def getZerosAndOnes():
+        #masking tensors with zeros and ones
+        #tensor range
+        pass
+    
+    def getTensorDataTypes():
+        #float , int , 16,32,64
+        #multiplying different data types and overflowing
+        pass
+    
+    def getTensorManipulation():
+        #add , sub,mul,divide
+        #Matrix Mul
+        #matrix transpose and mull
+        pass
+
+        
 
 def main():
     obj = Tensor101()
