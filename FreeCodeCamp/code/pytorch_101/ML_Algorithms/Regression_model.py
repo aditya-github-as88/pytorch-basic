4bf8173a91d2e9c1145523e1273d80f5d168fdf7
aditya-github-as88 <139616413+aditya-github-as88@users.noreply.github.com>
2023-08-16 01:28:01 +0530

    checking 16th Aug
    checking 16th Aug

--- FreeCodeCamp/code/pytorch_101/ML_Algorithms/Regression_model.py
+++ FreeCodeCamp/code/pytorch_101/ML_Algorithms/Regression_model.py
@@ -2,25 +2,99 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+import math
 
 class ModelRegression(object):
-    pass
-
     def plot_Linear(self):
-        path = "D:\GIT\Tech-Stack\Technology\Deep-Learning\Material\Links_notes\Algorithms\excel_docs\data_sets\home_prices.csv"
+        #fetch the data
+        path = "D:\GIT\Tech-Stack\Technology\Deep-Learning\pytorch-basic\Material\Links_notes\Algorithms\excel_docs\data_sets\home_prices.csv"
         df = pd.read_csv(path)
         print(df)
         
         print("\n\n")
         
-        plt.scatter(df.area,df.price)
+        #plot the data
+        plt.xlabel('area')
+        plt.ylabel('home_prices $')
+        plt.scatter(df.area,df.price, color = 'red' , marker = '+')
+        # plt.show()
+        
+        reg = linear_model.LinearRegression()
+        # reg.fit(df[['area']] , df.price)# traiing the LR model using data points
+        print(f'\n area is {df["area"]} \n')
+        print(f'\ntype of df area is {type(df[["area"]])}')
+        reg.fit(df[["area"]] , df["price"])
+        
+        
+        
+        # prediction - general static
+        x_pred = [[5000]]
+        y_pred =  reg.predict(x_pred)
+        print('\n predicted price for {0} is {1} \n'.format(x_pred,y_pred))
+        print('\n reg coeff {0}'.format(reg.coef_))
+        print('\n reg intercept {0}'.format(reg.intercept_))
+        
+        
+        # prediction from range of area from csv
+        path = "D:\GIT\Tech-Stack\Technology\Deep-Learning\pytorch-basic\Material\Links_notes\Algorithms\excel_docs\data_sets\Input-Area.csv"
+        df1 = pd.read_csv(path)
+        print(df1)
+        # input_area = df1
+        price_prediction =  reg.predict(df1)
+        print(f'\n ___ prediction analysis_____\n')
+        print(f'input area \n {df1}')
+        print(f'price predicted \n {price_prediction}')
+        
+        # adding predicted prices to data frame
+        df1['prices-predicted'] = price_prediction
+        print(f'\n Data Frame for predicted prices \n {df1}\n')
+        
+        #ploltting the prediction
+        plt.scatter(df1.area,df1["prices-predicted"], color = 'blue' , marker = '*')
         plt.show()
+        
+        
+        # saving data frame to csv file
+        output_path = "D:\GIT\Tech-Stack\Technology\Deep-Learning\pytorch-basic\Material\Links_notes\Algorithms\excel_docs\data_sets\Predictions\Prediction-Price.csv"
+        df1.to_csv(output_path , index=False)
     
-    
+    def plot_multi_Variable(self):
+        # price = m1 * area + m2 * bedrooms + m3 * age + b
+        
+         #fetch the data
+        path = "D:\GIT\Tech-Stack\Technology\Deep-Learning\pytorch-basic\Material\Links_notes\Algorithms\excel_docs\data_sets\multivar_data.csv"
+        df = pd.read_csv(path)
+        
+        print(f'\nMultivariable data set\n {df}\n')
+        
+        # handling null values via median
+        # calculate median
+        median_bedroom = math.floor(df.bedroom.median())
+        print(f'\nmedian_bedroom\n {median_bedroom}\n')
+        
+        #filling NA with median
+        df.bedroom = df.bedroom.fillna(median_bedroom)
+        # print(f'\n new data is \n {df} \n')
+        print(f'\n processed data after removing null values \n{df}\n')
+        
+        reg = linear_model.LinearRegression()
+        reg.fit(df[['area','bedroom','age']] , df['price'])
+        
+        # finding the coeeficients
+        print(f'\n  Coefficients for the model \n{reg.coef_}\n')
+        
+        #intercept
+        print(f'\n  intercept for the model \n{reg.intercept_}\n')
+        
+        #prediction
+        print(f'\n Prediction for the learned model is {reg.predict([[3000,3,40]])} \n')
+        
+        
     
 def main():
     obj = ModelRegression()
-    obj.plot_Linear()
+    # obj.plot_Linear()
+    obj.plot_multi_Variable()
     
 if __name__ == '__main__':
     main()
---
+++ FreeCodeCamp/code/pytorch_101/tensor103.py
@@ -0,0 +1,50 @@
+from sklearn.datasets import make_circles
+import pandas as pd
+import matplotlib.pyplot as plt
+
+"classification problem is a problem of predicting whether something is one thing or another and there can be multiple things as the options"
+
+class Tensor102(object):
+    def __init__(self,samples=1000,noise = 0.03 , random_seed = 42):
+        self.samples = samples or 1000
+        self.noise = noise or 0.4
+        self.random_seed = random_seed or 42
+        self.X = None
+        self.y = None
+        self.circles = None
+        
+    def createCircle(self):
+        self.X,self.y = make_circles(self.samples , noise= self.noise , random_state= self.random_seed)
+        
+    def getXY(self, top = 10):    
+        print(f"First {top} X features:\n{self.X[:top]}")
+        print(f"First {top} y features:\n{self.y[:top]}")
+        
+    def getCircles(self , head =10):
+        print(f"First {head} of circles are :\n{self.circles.head(head)}")
+        
+    def createDataFrame(self):
+        self.circles = pd.DataFrame({"X1": self.X[:, 0],
+                                    "X2": self.X[:, 1],
+                                    "label": self.y
+                                    })
+        
+        
+    def plotCircle(self):
+        plt.scatter(x=self.X[:, 0], 
+            y=self.X[:, 1], 
+            c=self.y, 
+            cmap=plt.cm.RdYlBu)
+        plt.show()
+        
+def main():
+    obj = Tensor102()
+    obj.createCircle()
+    obj.getXY()
+    obj.createDataFrame()
+    obj.getCircles()
+    obj.plotCircle()
+    
+    
+if __name__ == '__main__':
+    main()
